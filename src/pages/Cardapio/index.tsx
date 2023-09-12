6cb8d8b130256df8334b7a7199faddc2d5989cb5
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-12 08:35:32 -0300

    Upload inicial

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import style from './Cardapio.module.scss';
+
+function Cardapio() {
+    return (
+        <main>
+            <nav className={style.menu}>
+                
+            </nav>
+        </main>
+    );
+}
+
+export default Cardapio;
