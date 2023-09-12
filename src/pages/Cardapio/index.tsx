9c1162ca9ec331ffb5a1b353718d2d37ced6ca95
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-12 08:51:29 -0300

    Importando logo como SVG

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import style from './Cardapio.module.scss';
 
 function Cardapio() {
     return (
         <main>
             <nav className={style.menu}>
-                
+               <Logo/> 
             </nav>
         </main>
     );
