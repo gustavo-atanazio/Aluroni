fb3278ad79acc50683d6da8c359fcf355c3923bb
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-13 09:31:25 -0300

    Estruturando header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,12 @@ function Cardapio() {
             <nav className={style.menu}>
                <Logo/> 
             </nav>
+
+            <header className={style.header}>
+                <div className={style.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
         </main>
     );
 }
