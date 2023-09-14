ee2f24ba9f411216ea6538ca7a64bcd924373a8c
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-14 08:25:41 -0300

    Componente Buscador

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,24 @@
+import { CgSearch } from 'react-icons/cg';
+import style from './Buscador.module.scss';
+
+interface BuscadorProps {
+    busca: string
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+function Buscador({ busca, setBusca }: BuscadorProps) {
+    return (
+        <div className={style.buscador}>
+            <input
+                value={busca}
+                onChange={event => setBusca(event.target.value)}
+            />
+            <CgSearch
+                size={20}
+                color='#4C4D5E'
+            />
+        </div>
+    );
+}
+
+export default Buscador;
