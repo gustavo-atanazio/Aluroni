e9232f74b16b26ba4d011dcf1a81a575ee313bae
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-14 08:25:21 -0300

    Criando state

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import Buscador from './Buscador';
 import style from './Cardapio.module.scss';
 
 function Cardapio() {
+    const [busca, setBusca] = useState('');
+
     return (
         <main>
             <nav className={style.menu}>
@@ -13,6 +17,14 @@ function Cardapio() {
                     A casa do código e da massa
                 </div>
             </header>
+
+            <section className={style.cardapio}>
+                <h3 className={style.cardapio__titulo}>Cardápio</h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca}
+                />
+            </section>
         </main>
     );
 }
