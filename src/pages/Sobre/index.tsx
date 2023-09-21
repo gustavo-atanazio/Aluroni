543d3ad5484a45a7b87a9192ac4bb06c5bb7d9d6
gustavo-atanazio <gus.atanazio@gmail.com>
2023-09-21 09:16:03 -0300

    Terminando a tela Sobre

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,9 +1,42 @@
-// import style from './Sobre.module.scss';
+import casa from 'assets/sobre/casa.png';
+import massa1 from 'assets/sobre/massa1.png';
+import massa2 from 'assets/sobre/massa2.png';
+
+import styleTema from 'styles/Tema.module.scss';
+import style from './Sobre.module.scss';
+
+const imagens = [massa1, massa2];
 
 function Sobre() {
     return (
         <section>
-            Sobre
+            <h3 className={styleTema.titulo}>Sobre</h3>
+            <div className={style.sobreNos}>
+                <img src={casa} alt="Casa Aluroni"/>
+
+                <div className={style.sobreNos__texto}>
+                    <p>
+                        Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais 
+                        saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária 
+                        Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+                    </p>
+                    <p>
+                        Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+                    </p>
+                    <p>
+                        Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, 
+                        que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+                    </p>
+                </div>
+            </div>
+
+            <div className={style.imagens}>
+                {imagens.map((imagem, index) => (
+                    <div className={style.imagens__imagem} key={index}>
+                        <img src={imagem} alt="Massa"/>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 }
