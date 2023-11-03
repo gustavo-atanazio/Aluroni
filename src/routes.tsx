758ea784b9c7200dbef0f2210b5853a66eac3ce8
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-03 10:17:23 -0300

    Utilizando lazy loading (dynamic import)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Menu from 'components/Menu';
@@ -5,26 +6,30 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 import Inicio from 'pages/Inicio';
-import Cardapio from 'pages/Cardapio';
+// import Cardapio from 'pages/Cardapio';
 import Sobre from 'pages/Sobre';
 import Prato from 'pages/Prato';
 import NotFound from 'pages/NotFound';
+
+const Cardapio = lazy(() => import('pages/Cardapio'));
 
 function AppRouter() {
     return (
         <main className='container'>
             <Router>
-                <Menu/>
-                <Routes>
-                    <Route path='/' element={<Header/>}>
-                        <Route index element={<Inicio/>}/>
-                        <Route path='cardapio' element={<Cardapio/>}/>
-                        <Route path='sobre' element={<Sobre/>}/>
-                    </Route>
-                    <Route path='prato/:id' element={<Prato/>}/>
-                    <Route path='*' element={<NotFound/>}/>
-                </Routes>
-                <Footer/>
+                <Suspense fallback={<p>Carregando...</p>}>
+                    <Menu/>
+                    <Routes>
+                        <Route path='/' element={<Header/>}>
+                            <Route index element={<Inicio/>}/>
+                            <Route path='cardapio' element={<Cardapio/>}/>
+                            <Route path='sobre' element={<Sobre/>}/>
+                        </Route>
+                        <Route path='prato/:id' element={<Prato/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                    <Footer/>
+                </Suspense>
             </Router>
         </main>
     );
